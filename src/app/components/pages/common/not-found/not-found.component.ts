c6d882c5836f0e9416ee5926f3bbfa02c449f473
Miguel Ángel Vázquez Vera <vazquezvera.ma@gmail.com>
2020-08-12 14:39:39 +0200

    private and public menus working

--- src/app/components/pages/common/not-found/not-found.component.ts
+++ src/app/components/pages/common/not-found/not-found.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.scss']
+  styleUrls: ['./not-found.component.scss'],
 })
 export class NotFoundComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    setTimeout(() => {
+      console.log('Te redirige a Home o Dashboard');
+    }, 3000);
   }
-
 }
