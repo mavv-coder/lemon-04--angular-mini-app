44fcc24629c9ee9f58c0aabb01f3d688a46417e0
Miguel Ángel Vázquez Vera <vazquezvera.ma@gmail.com>
2020-08-12 14:15:28 +0200

    logo working

--- src/app/components/layout/private-menu/private-menu.component.ts
+++ src/app/components/layout/private-menu/private-menu.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-private-menu',
   templateUrl: './private-menu.component.html',
-  styleUrls: ['./private-menu.component.scss']
+  styleUrls: ['./private-menu.component.scss'],
 })
 export class PrivateMenuComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  urlLogo: string;
+  constructor() {
+    this.urlLogo = '../../../assets/img/logo.png';
   }
 
+  ngOnInit(): void {}
 }
