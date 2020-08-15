9c7d8eda26949684bdb9186173bee26c8dee695f
Miguel Ángel Vázquez Vera <vazquezvera.ma@gmail.com>
2020-08-15 17:43:22 +0200

    play btn working

--- src/app/components/pages/private/gallery/gallery.component.ts
+++ src/app/components/pages/private/gallery/gallery.component.ts
@@ -11,6 +11,7 @@ export class GalleryComponent {
   pictureList: ApiPictureEntity[] = [];
   currentScale: number = 1;
   currentPicture: ApiPictureEntity;
+  isPlayingGallery: boolean = false;
 
   constructor(public galleryService: GalleryService) {
     this.galleryService.getApiPictureListPromise().then((list) => {
@@ -27,10 +28,35 @@ export class GalleryComponent {
     this.currentScale = this.currentScale - 1;
   }
 
+  checkIfLastIndex(): boolean {
+    return (
+      this.currentPicture.id ===
+      this.pictureList[this.pictureList.length - 1].id
+    );
+  }
+
   handleNextPic(): void {
-    const index =
-      this.pictureList.findIndex((el) => el.id === this.currentPicture.id) + 1;
-    this.setCurrentPicture(this.pictureList[index]);
+    if (this.checkIfLastIndex()) {
+      this.setCurrentPicture(this.pictureList[0]);
+    } else {
+      const index =
+        this.pictureList.findIndex((el) => el.id === this.currentPicture.id) +
+        1;
+      this.setCurrentPicture(this.pictureList[index]);
+    }
+  }
+
+  handlePlayGallery(): void {
+    if (!this.isPlayingGallery) {
+      this.isPlayingGallery = true;
+      var playGallery = setInterval(() => {
+        this.handleNextPic();
+      }, 2000);
+    } else {
+      this.isPlayingGallery = false;
+      console.log('stop');
+      clearInterval(playGallery);
+    }
   }
 
   handlePreviousPic(): void {
