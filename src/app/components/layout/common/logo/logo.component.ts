067276979ff27b99dbed116202dd2e2fa7039967
Miguel Ángel Vázquez Vera <vazquezvera.ma@gmail.com>
2020-08-17 17:06:15 +0200

    logo updated

--- src/app/components/layout/common/logo/logo.component.ts
+++ src/app/components/layout/common/logo/logo.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-logo',
   templateUrl: './logo.component.html',
   styleUrls: ['./logo.component.scss'],
 })
-export class LogoComponent implements OnInit {
+export class LogoComponent {
   urlLogo: string;
+
   constructor() {
     this.urlLogo = 'assets/img/logo.png';
   }
-
-  ngOnInit(): void {}
 }
